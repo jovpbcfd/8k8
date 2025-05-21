c2fb0a656de4d4c88f540d9855865bb5ea7ccc3b
Jovan Lanutan <jovan.lanutan@cwti.local>
2025-05-21 14:23:58 +0800

    Fix build error

--- app/blogs/_components/other-post.tsx
+++ app/blogs/_components/other-post.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
-import Image from "next/image";
-import { urlFor } from "@/lib/sanity-client";
+// import Image from "next/image";
+// import { urlFor } from "@/lib/sanity-client";
 import { getFormattedDate } from "@/lib/utils";
 
 type Post = {
